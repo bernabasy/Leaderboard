36acacdd4ef50702f995ab8bb47b3485d94a1616
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-01-04 12:13:12 +0300

    Update index.js file to add Events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import { addScore } from './modules/Addinput.js';
+import { fetchScores } from './modules/featch.js';
+
+const form = document.querySelector('.form');
+const refresh = document.querySelector('.refresh');
+
+form.addEventListener('submit', addScore);
+refresh.addEventListener('click', fetchScores);
